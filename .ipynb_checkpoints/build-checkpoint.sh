1eac5d3c21f7eaffe995dd6380be00cfacf15fe3
TauInnov <yaminb@tauex.tau.ac.il>
2024-03-21 17:28:39 +0200

    test commit

---
+++ .ipynb_checkpoints/build-checkpoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if Jupyter Book is installed
+if ! command -v jupyter-book &> /dev/null
+then
+    echo "Jupyter Book is not installed. Please install Jupyter Book first."
+    exit
+fi
+
+# Build the Jupyter Book
+echo "Building Jupyter Book..."
+cd ../
+jupyter-book build newCourse
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Jupyter Book has been successfully built."
+else
+    echo "Failed to build Jupyter Book."
+fi
---
+++ scripts/commit_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if there are any changes to add
+if [[ -n $(git status -s) ]]; then
+    # Add all changes
+    git add .
+    git reset -- _build
+
+    # Prompt for commit message
+    read -p "Enter commit message: " commit_message
+
+    # Commit changes
+    git commit -m "$commit_message"
+
+    # Push changes to remote repository
+    git push origin main  # Change 'main' to your branch name if different
+else
+    echo "No changes to commit."
+fi
