9d958ba2f64dfd3d1605473303772f7a98abaf99
yaminbs <yaminbs@gmail.com>
2024-03-24 22:31:00 +0200

    more changes to Welcome page

---
+++ scripts/.ipynb_checkpoints/commit_push-checkpoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if there are any changes to add
+if [[ -n $(git status -s) ]]; then
+    # Add all changes
+    git add .
+    git reset -- _build
+
+    # Prompt for commit message
+    read -p "Enter commit message: " commit_message
+
+    # Commit changes
+    git commit -m "$commit_message"
+
+    #pull changes                                                               
+    git config pull.rebase false                                                
+    git pull 
+
+    # Push changes to remote repository
+    git push origin main  # Change 'main' to your branch name if different
+else
+    echo "No changes to commit."
+fi
